9f035d19fde60c4ee9621295bb08456834cf4997
Philae <philae.free@gmail.com>
2016-03-02 00:16:57 +0100

    edited blog view

--- web/urls.py
+++ web/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'web'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^blog/$', views.blog, name='blog'),
+    url(r'^blog/$', views.BlogView.as_view(), name='blog'),
     url(r'^blog/(?P<pk>[0-9]+)/$', views.blog_single, name='blog_single'),
     url(r'^portfolio/$', views.portfolio, name='portfolio'),
     url(r'^contact/$', views.contact, name='contact'),
--- web/views.py
+++ web/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.views import generic
 
 # Create your views here.
 from web.models import Me
 
+
+class BlogView(generic.ListView):
+    template_name = 'web/blog.html'
+    context_object_name = 'blog_posts'
+
+    def get_queryset(self):
+        return Blog.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
 
 def index(request):
     me = Me.objects.all()[0]
